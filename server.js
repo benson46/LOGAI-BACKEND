f53f44ebb8618faf4e351c272c95041d06f731ab
Benson B Varoor <bensonbaiju46@gmail.com>
2025-05-24 16:32:09 +0530

    hosting complete

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [process.env.CORS_ORIGIN, process.env.CORS_ORIGIN2],
   methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
 }));
 
 app.use(express.json());
