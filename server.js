3a1f63a9b125ed3d64f790f920c5ea0103145d61
Benson B Varoor <bensonbaiju46@gmail.com>
2025-05-24 16:18:41 +0530

    testing-hosting

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors());
+app.use(cors({
+  origin:[process.env.CORS_ORIGIN,process.env.CORS_ORIGIN2]
+}));
 app.use(express.json());
 
 // routes
